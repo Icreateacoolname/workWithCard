c9d58ad4bc54ed61bf29553e02a6857af026acbb
Alexander Aleshin <hichrov@gmail.com>
2021-03-28 02:19:13 +0300

    add new field with 3 cards on it, some problems with animation

---
+++ app.js
@@ -0,0 +1,18 @@
+const easy = document.querySelector('.easy');
+const medium = document.querySelector('.medium');
+const hard = document.querySelector('.hard');
+const btn = document.querySelector('.startGame');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
